0321e938bc04234bfa035ae65217bdb606838a85
eyurenalison <eeyuren1@gmail.com>
2025-07-24 20:34:26 +0100

    Fix: Removed cron folder form app/src/api

--- src/app/api/cron/wedding-reminders/route.ts
+++
@@ -1,6 +0,0 @@
-import { runWeddingReminderCron } from "../../../../../cron/reminders";
-
-export async function GET() {
-  await runWeddingReminderCron();
-  return new Response("Wedding cron executed", { status: 200 });
-}
